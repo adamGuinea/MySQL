1fa497715809abcb56b1812c2772f9e269969ce0
Adam Guinea <adamguinea22@gmail.com>
2019-02-21 23:47:28 +0000

    uncommented statements

--- LogicalOperators/data_set.sql
+++ LogicalOperators/data_set.sql
@@ -1,53 +1,53 @@
--- -- -- SELECT
--- -- --     title, stock_quantity, author_lname, author_fname
--- -- -- FROM books WHERE stock_quantity < 200; 
+SELECT
+    title, stock_quantity, author_lname, author_fname
+FROM books WHERE stock_quantity < 200; 
 
--- -- -- SELECT
--- -- --     title
--- -- -- FROM books WHERE title NOT LIKE 'W%';
+SELECT
+    title
+FROM books WHERE title NOT LIKE 'W%';
 
--- -- SELECT 
--- --     title, released_year
--- -- FROM books WHERE released_year > 1991;
+SELECT 
+    title, released_year
+FROM books WHERE released_year > 1991;
 
--- -- SELECT 
--- --     title, author_lname, released_year
--- -- FROM books WHERE released_year > 2010 && author_lname = 'Eggers';
+SELECT 
+    title, author_lname, released_year
+FROM books WHERE released_year > 2010 && author_lname = 'Eggers';
 
--- -- SELECT          
--- --     *
--- -- FROM products WHERE brand = 'lego' && instock = true;
+SELECT          
+    *
+FROM products WHERE brand = 'lego' && instock = true;
 
--- -- SELECT
--- --     *
--- -- FROM books WHERE author_lname = 'Eggers' && released_year || 2010 && title LIKE '%novel%';
+SELECT
+    *
+FROM books WHERE author_lname = 'Eggers' && released_year || 2010 && title LIKE '%novel%';
 
--- SELECT
---     title, released_year
--- FROM books BETWEEN 2004 AND 2015;
+SELECT
+    title, released_year
+FROM books BETWEEN 2004 AND 2015;
 
--- SELECT
---     title, released_year
--- FROM books NOT BETWEEN 2004 AND 2015;
+SELECT
+    title, released_year
+FROM books NOT BETWEEN 2004 AND 2015;
 
--- SELECT CAST('2019-02-22' AS DATETIME);
+SELECT CAST('2019-02-22' AS DATETIME);
 
--- SELECT name, birthdt FROM people
--- WHERE birthdt BETWEEN CAST('1991-01-30' AS DATETIME) 
---                       AND CAST('1993-02-18' AS DATETIME);
+SELECT name, birthdt FROM people
+WHERE birthdt BETWEEN CAST('1991-01-30' AS DATETIME) 
+                      AND CAST('1993-02-18' AS DATETIME);
                       
--- SELECT 
---     title, author_lname 
--- FROM books WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+SELECT 
+    title, author_lname 
+FROM books WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
 
--- SELECT 
---     title, released_year
--- FROM books WHERE released_year IN (2017, 1995);
+SELECT 
+    title, released_year
+FROM books WHERE released_year IN (2017, 1995);
 
--- SELECT 
---     title, released_year
--- FROM books WHERE released_year >= 2000
--- AND released_year % 2 != 0;
+SELECT 
+    title, released_year
+FROM books WHERE released_year >= 2000
+AND released_year % 2 != 0;
 
 -- Conditionals
 
